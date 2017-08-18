8ecce14df5564d32e17d9e8e08362fba29b5ce1a
asbis92 <rdsl_ivanov@yahoo.com>
2017-08-18 16:46:21 +0300

    ToDoListApplication - PHP, JS, Java, C#

---
+++ C# Skeleton/TodoList/Controllers/TaskController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using TodoList.Models;
+
+namespace TodoList.Controllers
+{
+	public class TaskController : Controller
+	{
+	    [HttpGet]
+        [Route("")]
+	    public ActionResult Index()
+	    {
+            //TODO: Implement me...
+            using (var database = new TodoListDbContext())
+            {
+                var tasks = database.Tasks.ToList();
+                return View(tasks);
+            }
+                
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+		{
+            return View();
+
+        }
+
+		[HttpPost]
+		[Route("create")]
+        [ValidateAntiForgeryToken]
+		public ActionResult Create(Task task)
+		{
+            using (var database = new TodoListDbContext())
+            {
+                database.Tasks.Add(task);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                
+        }
+
+		[HttpGet]
+		[Route("delete/{id}")]
+        public ActionResult Delete(int id)
+		{
+            using (var database = new TodoListDbContext())
+            {
+                var task = database.Tasks.Find(id);
+                return View(task);
+            }
+                
+        }
+
+		[HttpPost]
+		[Route("delete/{id}")]
+        [ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirm(int id)
+		{
+            using (var database = new TodoListDbContext())
+            {
+                var task = database.Tasks.Find(id);
+                database.Tasks.Remove(task);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
+                
+        }
+	}
+}
---
+++ C# Skeleton/TodoList/Models/Task.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoList.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Comments { get; set; }
+
+
+
+    }
+}
